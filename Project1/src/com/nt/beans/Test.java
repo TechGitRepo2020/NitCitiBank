b16358fabb25bb7e1a25c9c7a8677e323b36675e
TL <TL@gmail.com>
2020-04-21 00:09:56 +0530

    dev1 some oprations

--- Project1/src/com/nt/beans/Test.java
+++ Project1/src/com/nt/beans/Test.java
@@ -1,5 +1,12 @@
 package com.nt.beans;
 
+import com.nt.test.Utility;
+
 public class Test {
+	public static void main(String[] args) {
+		Utility u=new Utility();
+		
+		System.out.println(u.getSysdate());
+	}
 
 }
